949b43f3530659e8d632ad0f862b58d3bc860749
shinorimax <syagi2@stanford.edu>
2025-05-14 19:37:50 -0700

    two-sample-testing

--- Analysis/phylodyn/dist_based_test.R
+++ Analysis/phylodyn/dist_based_test.R
@@ -1,8 +1,8 @@
 
 library(fmatrix)
 
-setwd("~/Documents/Paromita/")
-setwd("~/Documents/Paromita/Frechet_Trees/Rcode/")
+# setwd("~/Documents/Paromita/")
+# setwd("~/Documents/Paromita/Frechet_Trees/Rcode/")
 
 source("pMaxBB.R")
 
@@ -219,173 +219,173 @@ ret <- reth <- reto <-
 m_samplesize <- 1000
 n_tips <- 20
 
-
-set.seed(123)
-for(i in 1:B){
-
-  cat("\r", i)
-
-  xlist <- lapply(betas, gen_x, m = m_samplesize, n = n_tips)
-  nullx <- gen_x(0, m = m_samplesize, n = n_tips)
-
-  ilist <- lapply(xlist, function(u){
-    N <- nrow(u)
-    dist <- matrix(NA, N, N)
-    for(i in 1:N) {for (j in 1:i){
-      dist[i,j] <- dist[j,i] <-
-        mean((u[i,] - u[j])**2)}}
-    which.min(apply(dist,1,mean))
-  })
-
-  try({
-    ret[i, ] <- sapply(xlist, newTest, y = nullx)
-
-    reth[i, ] <- sapply(xlist, myHotelling, y = nullx)
-
-    reto[i, ] <- sapply(xlist, oldTest, y = nullx)
-
-    retg <- sapply(xlist, graphTest, y = nullx)
-
-    retg1[i, ] <- retg[1,]
-    retg2[i, ] <- retg[2,]
-    retg3[i, ] <- retg[3,]
-    retg4[i, ] <- retg[4,]
-  })
-}
-
-ALPHA <- .05
-
-
-fret_n <- ret < ALPHA
-fret_h <- reth
-fret_o <- reto > ALPHA
-fret_g1 <- retg1 < ALPHA
-fret_g2 <- retg1 < ALPHA
-fret_g3 <- retg1 < ALPHA
-fret_g4 <- retg1 < ALPHA
-
-
-
-library(ggplot2)
-
-geom_thisfret <- function(u, label){
-  list(geom_line(aes(y = apply(u, 2, mean, na.rm = TRUE),
-                     colour = label)),
-       geom_point(aes(y = apply(u, 2, mean, na.rm = TRUE),
-                      colour = label)))
-}
-colours <- c("Hotelling" = "black",
-             "New Test" = "red",
-             # "G1" = "yellow",
-             # "G2" = "orange",
-             # "G3" = "purple",
-             "Graph-based" = "brown")
-
-
-p <- ggplot(data = NULL, aes(x = betas)) +
-  labs(x = "beta", y = sprintf("Estimated power (%d replicates)", B),
-       title = "Test power, null = Kingman") +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5),
-        legend.position = "bottom") +
-  geom_hline(yintercept = .05, linetype = 2) +
-  geom_thisfret(fret_h, "Hotelling") +
-  geom_thisfret(fret_n, "New Test") +
-  # geom_thisfret(fret_g1, "G1") +
-  # geom_thisfret(fret_g2, "G2") +
-  # geom_thisfret(fret_g3, "G3") +
-  geom_thisfret(fret_g4, "Graph-based") +
-  scale_colour_manual(values = colours) +
-  xlim(c(NA, 1))+
-  NULL
-
-p
-# p <- ggplot(data = NULL, aes(x = betas, y = apply(ret2, 2, mean, na.rm = TRUE))) +
-#   geom_line() +
-#   geom_point() +
-#   geom_hline(yintercept = .05, linetype = 2) +
-#   labs(x = "beta", y = "Estimated power (5000 replicates)",
-#        title = "BB test power, null = Kingman") +
-#   theme_bw() +
-#   theme(plot.title = element_text(hjust = 0.5))
-# p
-# ggsave(p, filename = "power_BB.png")
-
-# p2 <- ggplot(data = NULL, aes(x = betas)) +
-#   geom_line(aes(y = apply(ret2, 2, mean, na.rm = TRUE)), colour = "red") +
-#   geom_point(aes(y = apply(ret2, 2, mean, na.rm = TRUE)), colour = "red") +
-#   geom_line(aes(y = apply(reth, 2, mean, na.rm = TRUE)), colour = "black") +
-#   geom_point(aes(y = apply(reth, 2, mean, na.rm = TRUE)), colour = "black") +
-#   geom_line(aes(y = apply(reto2, 2, mean, na.rm = TRUE)), colour = "blue") +
-#   geom_point(aes(y = apply(reto2, 2, mean, na.rm = TRUE)), colour = "blue") +
-#   geom_hline(yintercept = .05, linetype = 2) +
-#   labs(x = "beta", y = "Estimated power (500 replicates)",
-#        title = "Test power, null = Kingman") +
-#   theme_bw() +
-#   theme(plot.title = element_text(hjust = 0.5))
-# p2 + xlim(NA, 10)
-
-
-# save(list = ls(), file = "dist_based_test_20210824.RData")
-
-
-
-
-B <- 5000
-
-retnull <- matrix(nrow = B, ncol = 1)
-for(i in 1:B){
-
-  cat("\r", i)
-
-  nullx <- gen_x(0, m = 100, n = 7)
-  nully <- gen_x(0, m = 100, n = 7)
-
-  try({
-    retnull[i, ] <- newTest(nullx, nully, flag = "stat")
-  })
-}
-
-##Analysis of SARS-Cov-2 Washington State
-#Sample 1: All 170 sequences obtained from Gisaid, high coverage 2022-05-24
- #         These sequences were aligned with the following line:
-  #        mafft --thread -1 HCOV_Washington_2023_11_12_20.fasta > HCOV_Washingtont_out.fasta
-  #        The xml assumed mutation rate of .00074 per year: Washington_May_short.xml. 
-  #          Generated 5000000 posterior samples and thinned every 5000 to obtain 1001 samples
-          
-##Prepare F-matrices
-##Posterior distribution of trees
-library("ape")
-trees<-read.nexus(file="~/Documents/Covid/data/Washington_May_short-HCOV_Washingtont_out.trees")
-#trees1<-read.nexus(file="~/Documents/Covid/data/trees1.trees")
-length(trees)
-trees_small<-trees[seq(1,5001,by=5)]
-
-
-library("fmatrix")
-listF<-list()
-for (j in 1:length(trees_small)){
-  listF[[j]]<-gen_Fmat(trees_small[[j]])
-  print(j)
-  print(dim(listF[[j]]))
-  if (j==1){M<-listF[[j]]}
-  M<-M+listF[[j]]
-}
-
-saveRDS(listF,file="listF_May")
-M<-M/length(listF)
-listFx<-matrix_list(listF)
-listFx<-t(listFx)
-
-##Simulate from Kingman
-listFKing<-gen_x(0, m =1000 , n = 170)
-saveRDS(listFKing,file="listFKing")
-#test
-test_out<-newTest(listFx,listFKing)
-
-
-
-tree_mcc<-read.nexus(file="~/Documents/Covid/data/MCC_Washington_May")
+  # 
+  # set.seed(123)
+  # for(i in 1:B){
+  # 
+  #   cat("\r", i)
+  # 
+  #   xlist <- lapply(betas, gen_x, m = m_samplesize, n = n_tips)
+  #   nullx <- gen_x(0, m = m_samplesize, n = n_tips)
+  # 
+  #   ilist <- lapply(xlist, function(u){
+  #     N <- nrow(u)
+  #     dist <- matrix(NA, N, N)
+  #     for(i in 1:N) {for (j in 1:i){
+  #       dist[i,j] <- dist[j,i] <-
+  #         mean((u[i,] - u[j])**2)}}
+  #     which.min(apply(dist,1,mean))
+  #   })
+  # 
+  #   try({
+  #     ret[i, ] <- sapply(xlist, newTest, y = nullx)
+  # 
+  #     reth[i, ] <- sapply(xlist, myHotelling, y = nullx)
+  # 
+  #     reto[i, ] <- sapply(xlist, oldTest, y = nullx)
+  # 
+  #     retg <- sapply(xlist, graphTest, y = nullx)
+  # 
+  #     retg1[i, ] <- retg[1,]
+  #     retg2[i, ] <- retg[2,]
+  #     retg3[i, ] <- retg[3,]
+  #     retg4[i, ] <- retg[4,]
+  #   })
+  # }
+  # 
+  # ALPHA <- .05
+  # 
+  # 
+  # fret_n <- ret < ALPHA
+  # fret_h <- reth
+  # fret_o <- reto > ALPHA
+  # fret_g1 <- retg1 < ALPHA
+  # fret_g2 <- retg1 < ALPHA
+  # fret_g3 <- retg1 < ALPHA
+  # fret_g4 <- retg1 < ALPHA
+  # 
+  # 
+  # 
+  # library(ggplot2)
+  # 
+  # geom_thisfret <- function(u, label){
+  #   list(geom_line(aes(y = apply(u, 2, mean, na.rm = TRUE),
+  #                      colour = label)),
+  #        geom_point(aes(y = apply(u, 2, mean, na.rm = TRUE),
+  #                       colour = label)))
+  # }
+  # colours <- c("Hotelling" = "black",
+  #              "New Test" = "red",
+  #              # "G1" = "yellow",
+  #              # "G2" = "orange",
+  #              # "G3" = "purple",
+  #              "Graph-based" = "brown")
+  # 
+  # 
+  # p <- ggplot(data = NULL, aes(x = betas)) +
+  #   labs(x = "beta", y = sprintf("Estimated power (%d replicates)", B),
+  #        title = "Test power, null = Kingman") +
+  #   theme_bw() +
+  #   theme(plot.title = element_text(hjust = 0.5),
+  #         legend.position = "bottom") +
+  #   geom_hline(yintercept = .05, linetype = 2) +
+  #   geom_thisfret(fret_h, "Hotelling") +
+  #   geom_thisfret(fret_n, "New Test") +
+  #   # geom_thisfret(fret_g1, "G1") +
+  #   # geom_thisfret(fret_g2, "G2") +
+  #   # geom_thisfret(fret_g3, "G3") +
+  #   geom_thisfret(fret_g4, "Graph-based") +
+  #   scale_colour_manual(values = colours) +
+  #   xlim(c(NA, 1))+
+  #   NULL
+  # 
+  # p
+  # # p <- ggplot(data = NULL, aes(x = betas, y = apply(ret2, 2, mean, na.rm = TRUE))) +
+  # #   geom_line() +
+  # #   geom_point() +
+  # #   geom_hline(yintercept = .05, linetype = 2) +
+  # #   labs(x = "beta", y = "Estimated power (5000 replicates)",
+  # #        title = "BB test power, null = Kingman") +
+  # #   theme_bw() +
+  # #   theme(plot.title = element_text(hjust = 0.5))
+  # # p
+  # # ggsave(p, filename = "power_BB.png")
+  # 
+  # # p2 <- ggplot(data = NULL, aes(x = betas)) +
+  # #   geom_line(aes(y = apply(ret2, 2, mean, na.rm = TRUE)), colour = "red") +
+  # #   geom_point(aes(y = apply(ret2, 2, mean, na.rm = TRUE)), colour = "red") +
+  # #   geom_line(aes(y = apply(reth, 2, mean, na.rm = TRUE)), colour = "black") +
+  # #   geom_point(aes(y = apply(reth, 2, mean, na.rm = TRUE)), colour = "black") +
+  # #   geom_line(aes(y = apply(reto2, 2, mean, na.rm = TRUE)), colour = "blue") +
+  # #   geom_point(aes(y = apply(reto2, 2, mean, na.rm = TRUE)), colour = "blue") +
+  # #   geom_hline(yintercept = .05, linetype = 2) +
+  # #   labs(x = "beta", y = "Estimated power (500 replicates)",
+  # #        title = "Test power, null = Kingman") +
+  # #   theme_bw() +
+  # #   theme(plot.title = element_text(hjust = 0.5))
+  # # p2 + xlim(NA, 10)
+  # 
+  # 
+  # # save(list = ls(), file = "dist_based_test_20210824.RData")
+  # 
+  # 
+  # 
+  # 
+  # B <- 5000
+  # 
+  # retnull <- matrix(nrow = B, ncol = 1)
+  # for(i in 1:B){
+  # 
+  #   cat("\r", i)
+  # 
+  #   nullx <- gen_x(0, m = 100, n = 7)
+  #   nully <- gen_x(0, m = 100, n = 7)
+  # 
+  #   try({
+  #     retnull[i, ] <- newTest(nullx, nully, flag = "stat")
+  #   })
+  # }
+  # 
+  # ##Analysis of SARS-Cov-2 Washington State
+  # #Sample 1: All 170 sequences obtained from Gisaid, high coverage 2022-05-24
+  #  #         These sequences were aligned with the following line:
+  #   #        mafft --thread -1 HCOV_Washington_2023_11_12_20.fasta > HCOV_Washingtont_out.fasta
+  #   #        The xml assumed mutation rate of .00074 per year: Washington_May_short.xml. 
+  #   #          Generated 5000000 posterior samples and thinned every 5000 to obtain 1001 samples
+  #           
+  # ##Prepare F-matrices
+  # ##Posterior distribution of trees
+  # library("ape")
+  # trees<-read.nexus(file="~/Documents/Covid/data/Washington_May_short-HCOV_Washingtont_out.trees")
+  # #trees1<-read.nexus(file="~/Documents/Covid/data/trees1.trees")
+  # length(trees)
+  # trees_small<-trees[seq(1,5001,by=5)]
+  # 
+  # 
+  # library("fmatrix")
+  # listF<-list()
+  # for (j in 1:length(trees_small)){
+  #   listF[[j]]<-gen_Fmat(trees_small[[j]])
+  #   print(j)
+  #   print(dim(listF[[j]]))
+  #   if (j==1){M<-listF[[j]]}
+  #   M<-M+listF[[j]]
+  # }
+  # 
+  # saveRDS(listF,file="listF_May")
+  # M<-M/length(listF)
+  # listFx<-matrix_list(listF)
+  # listFx<-t(listFx)
+  # 
+  # ##Simulate from Kingman
+  # listFKing<-gen_x(0, m =1000 , n = 170)
+  # saveRDS(listFKing,file="listFKing")
+  # #test
+  # test_out<-newTest(listFx,listFKing)
+  # 
+  # 
+  # 
+  # tree_mcc<-read.nexus(file="~/Documents/Covid/data/MCC_Washington_May")
 
 
 
